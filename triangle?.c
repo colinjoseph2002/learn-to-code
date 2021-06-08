e182885ba856248c9ac61f7269823cd8d3cf64a4
colinjoseph2002 <83956753+colinjoseph2002@users.noreply.github.com>
2021-06-08 20:36:33 +0530

    Create triangle?.c

---
+++ triangle?.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if( (a+b>c)&&(a+c>b)&&(b+c>a))
+        printf("Yes");
+    else
+        printf("No");
+}
